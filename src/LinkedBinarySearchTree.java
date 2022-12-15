542ce32d0ada6edccfad73a04032cdec5e0068ad
k4rr3 <davidcarreras2003@gmail.com>
2022-12-15 12:10:29 +0100

    Update LinkedBinarySearchTree.java

--- src/LinkedBinarySearchTree.java
+++ src/LinkedBinarySearchTree.java
@@ -83,24 +83,62 @@ public class LinkedBinarySearchTree<K, V>
 
     private Node<K, V> createNewNode(Node<K, V> node, K key, V value) {
         if (node != null) {
+            //Si key o value son null lanza NullPointerException
             if (node.key == null || node.value == null) {
                 throw new NullPointerException("");
             }
+
             if (comparator.compare(node.key, key) < 0) {
                 return new Node<K, V>(node.key, node.value, node.left, createNewNode(node.right, key, value));
             } else if (comparator.compare(node.key, key) > 0) {
                 return new Node<K, V>(node.key, node.value, createNewNode(node.left, key, value), node.right);
             } else {
+//                Si key existe en el ABB de origen se modificará su actual valor por
+//                value generando un nuevo nodo que represente esta modificación
+//                para el árbol devuelto.
                 return new Node<K, V>(node.key, value, node.left, node.right);
             }
         }
         //throw new NullPointerException("");
+        //Si el arbol que teniamos estaba vacio, crearemos un nuevo nodo que será la raíz
         return new Node<K, V>(key, value);
     }
 
     @Override
     public LinkedBinarySearchTree<K, V> remove(K key) {
 // ¿?
+        if (key == null) {
+            throw new NullPointerException("");
+        }
+        if (containsKey(key)) {
+            return new LinkedBinarySearchTree<>(comparator, deleteSpecificNode(root, key));
+        }
         return new LinkedBinarySearchTree<>(comparator, root);
+    }
+
+    private Node<K, V> deleteSpecificNode(Node<K, V> node, K key) {
+        if (node != null) {
+            //Si key o value son null lanza NullPointerException
+//            if (node.key == null || node.value == null) {
+//                throw new NullPointerException("");
+//            }
+            Node<K, V> leftNode = node.left;
+            Node<K, V> rightNode = node.right;
+            if (leftNode != null && leftNode.key == key) {
+                leftNode.left =
+                return leftNode.left;
+            } else if (rightNode != null && rightNode.key == key) {
+                return rightNode.left;
+            }
+            if (comparator.compare(node.key, key) < 0) {
+                return new Node<K, V>(node.key, node.value, leftNode, deleteSpecificNode(rightNode, key));
+            } else if (comparator.compare(node.key, key) > 0) {
+                return new Node<K, V>(node.key, node.value, deleteSpecificNode(leftNode, key), rightNode);
+            } else {
+//                Si key existe en el ABB
+                if (leftNode.key == null)
+                    return new Node<K, V>(null, null);
+            }
+        }
     }
 }
