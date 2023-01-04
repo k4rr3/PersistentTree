5a8346ef89459b71d7b47583e18aa564fae100e6
k4rr3 <davidcarreras2003@gmail.com>
2023-01-04 18:50:20 +0100

    Inorder iterative algorithm implemented with contexts and enums (also with an optimized version)

--- src/PersistentTree/Traversals.java
+++ src/PersistentTree/Traversals.java
@@ -5,8 +5,18 @@ import java.util.*;
 public class Traversals {
     public static <E> List<E> inorder(BinaryTree<E> tree) {
         List<E> result = new ArrayList<>();
+            //RECURSIVE INORDER ALGORITHM
         //inorderRec(result, tree);
-        result = inorderIter(tree);
+
+            //ITERATIVE INORDER ALGORITHM WITH CONTEXTS AND ENUMS
+        //result = inorderIter(tree);
+
+            //OPTIMIZED ITERATIVE INORDER ALGORITHM WITH CONTEXTS AND ENUMS
+        result = inorderIterOptimized(tree);
+
+            //ITERATIVE INORDER ALGORITHM WITHOUT CONTEXTS AND ENUMS
+        //result = inorderIterWithoutContext(tree);
+
         return result;
     }
 
@@ -19,7 +29,102 @@ public class Traversals {
         }
     }
 
-    public static <E> List<E> inorderIter(BinaryTree<E> tree) {
+    private static class Context<E> {
+        BinaryTree<E> tree;
+        EntryPoint entryPoint;
+
+        public Context(BinaryTree<E> tree, EntryPoint entryPoint) {
+            this.tree = tree;
+            this.entryPoint = entryPoint;
+        }
+    }
+
+    enum EntryPoint {
+        CALL, RESUME, RESUME2
+    }
+  public static <E> List<E> inorderIter(BinaryTree<E> tree) {
+        List<E> result = new ArrayList<>();
+        Deque<Context> stack = new ArrayDeque<>();
+
+        if (tree.isEmpty()) {
+            return result;
+        }
+
+        stack.addFirst(new Context(tree, EntryPoint.CALL));
+        while (!stack.isEmpty()) {
+            var context = stack.removeFirst();
+            switch (context.entryPoint) {
+                case CALL -> {
+                    var actual = context.tree;
+                    while (!actual.isEmpty()) {
+                        stack.addFirst(new Context(actual, EntryPoint.RESUME));
+                        actual = actual.left();
+                    }
+                }
+                case RESUME -> {
+                    result.add((E) context.tree.root());
+                    if (context.tree.right() != null && !context.tree.right().isEmpty()) {
+                        stack.addFirst(new Context(context.tree.right(), EntryPoint.RESUME2));
+                    }
+                }
+                case RESUME2 -> {
+                    var current = context.tree;
+                    while (!current.isEmpty()) {
+                        stack.addFirst(new Context(current, EntryPoint.RESUME));
+                        current = current.left();
+                    }
+                }
+            }
+        }
+        return result;
+    }
+
+    private static class ContextOptimized<E> {
+        BinaryTree<E> tree;
+        EntryPointOptimized entryPoint;
+
+        public ContextOptimized(BinaryTree<E> tree, EntryPointOptimized entryPoint) {
+            this.tree = tree;
+            this.entryPoint = entryPoint;
+        }
+    }
+
+    enum EntryPointOptimized {
+        CALL, RESUME
+    }
+
+    public static <E> List<E> inorderIterOptimized(BinaryTree<E> tree) {
+        List<E> result = new ArrayList<>();
+        Deque<ContextOptimized> stack = new ArrayDeque<>();
+
+        if (tree.isEmpty()) {
+            return result;
+        }
+
+        stack.addFirst(new ContextOptimized(tree, EntryPointOptimized.CALL));
+        while (!stack.isEmpty()) {
+            var context = stack.removeFirst();
+            switch (context.entryPoint) {
+                case CALL -> {
+                    var actual = context.tree;
+                    while (!actual.isEmpty()) {
+                        stack.addFirst(new ContextOptimized(actual, EntryPointOptimized.RESUME));
+                        actual = actual.left();
+                    }
+                }
+                case RESUME -> {
+                    result.add((E) context.tree.root());
+                    if (context.tree.right() != null && !context.tree.right().isEmpty()) {
+                        stack.addFirst(new ContextOptimized(context.tree.right(), EntryPointOptimized.CALL));
+                    }
+                }
+            }
+        }
+        return result;
+    }
+
+
+     public static <E> List<E> inorderIterWithoutContext(BinaryTree<E> tree) {
         List<E> result = new ArrayList<>();
         Deque<BinaryTree<E>> stack = new ArrayDeque<>();
 
